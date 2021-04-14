d5e64aac6e0179b9bc77a62c5962b2316b7c89e3
hoanghongminh <hoangminh881997@gmail.com>
2021-04-14 11:10:09 +0700

    test

--- Web/Web/src/components/layout/TheContent.vue
+++ Web/Web/src/components/layout/TheContent.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <!-- add router-view để thực hiện load trang store -->
+<!-- add router-view để thực hiện load trang store -->
+<!-- add router-view để thực hiện load trang store -->
     <router-view></router-view>
   </div>
 </template>
